7b482f5d31596f625901deed6c761596432ae519
Martin <mara0011@stud.kea.dk>
2026-08-07 12:58:46 +0200

    Tilføj Astro-løsning til Høst Møn

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface Kunstner {
+  id: number;
+  slug: string;
+  navn: string;
+  spilledag: string;
+  spilledato: string;
+  spilletid: string;
+  beskrivelse: string;
+  billede_url: string;
+  sortering: number;
+}
